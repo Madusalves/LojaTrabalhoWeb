78f3a6f2d5591365a51363483ebfda314ef43f98
Madusalves <malvesduarte444@gmail.com>
2024-06-19 22:04:39 -0300

    Classe Db Factory

---
+++ Data/DbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaTrabalhoWeb.Data
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<LojaDbContext>
+    {
+        public LojaDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LojaDbContext>();
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConection");
+            optionsBuilder.UseMySql(connectionString, new MySqlServerVersion((new Version (8,0,26))));
+
+            return new LojaDbContext(optionsBuilder.Options);
+        }
+
+        public
+    }
+}
