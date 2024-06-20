ee4a2e38c2662350ab6819bdf35993253a5541d0
Madusalves <malvesduarte444@gmail.com>
2024-06-19 21:46:05 -0300

    Implementando a Classe Produto

---
+++ Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace LojaTrabalhoWeb.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Double Preco { get; set; }
+        
+    }
+}
