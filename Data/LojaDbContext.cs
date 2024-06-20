88404c7109ceb5304c8ee5605353cf820b062340
Madusalves <malvesduarte444@gmail.com>
2024-06-19 21:49:48 -0300

    Conectando o DbContext

---
+++ Data/LojaDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LojaTrabalhoWeb.Data
+{
+    public class LojaDbContext : DbContext
+    {
+        public LojaDbContext(DbContextOptions<LojaDbContext> options) : base(options) { }
+        public DbSet<ProducesResponseTypeMetadata> Produto { get; set; }
+    }
+}
