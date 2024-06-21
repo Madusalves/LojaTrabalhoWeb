54327037fc088bb1fb6bb703fffd10b65d442e2c
Madusalves <malvesduarte444@gmail.com>
2024-06-21 20:53:05 -0300

    Funcionalidade Deposito

--- Models/Deposito.cs
+++ Models/Deposito.cs
@@ -1,6 +1,18 @@
-﻿namespace LojaTrabalhoWeb.Models
+﻿using System.Collections.Generic;
+
+namespace LojaTrabalhoWeb.Models
 {
     public class Deposito
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public List<Produto> Produtos { get; set; }
+
+        public Deposito(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+            Produtos = new List<Produto>();
+        }
     }
 }
