06c48142bce40c22d85e74d1730b6b3de303c660
Madusalves <malvesduarte444@gmail.com>
2024-06-21 22:14:54 -0300

    Deposito service

--- Services/DepositoService.cs
+++ Services/DepositoService.cs
@@ -1,6 +1,32 @@
-﻿namespace LojaTrabalhoWeb.Services
+﻿using LojaTrabalhoWeb.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LojaTrabalhoWeb.Services
 {
     public class DepositoService
     {
+        private readonly Deposito deposito;
+
+        public DepositoService(Deposito deposito)
+        {
+            this.deposito = deposito;
+        }
+
+        public void AdicionarProduto(int idProduto, string nomeProduto, int quantidade)
+        {
+            var produto = new Produto(idProduto, nomeProduto);
+            deposito.AdicionarProduto(produto, quantidade);
+        }
+
+        public List<Produto> ObterProdutos()
+        {
+            return deposito.Produtos;
+        }
+
+        public Produto ObterProdutoPorId(int idProduto)
+        {
+            return deposito.Produtos.FirstOrDefault(p => p.Id == idProduto);
+        }
     }
 }
